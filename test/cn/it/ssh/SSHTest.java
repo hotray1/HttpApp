691d7e6f80eb6f784e35de538d12c31475e530e2
zhang <32723988@qq.com>
2015-08-31 15:00:44 +0800

    7 Spring环境搭建

---
+++ test/cn/it/ssh/SSHTest.java
@@ -0,0 +1,23 @@
+package cn.it.ssh;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:applicationContext-*.xml")
+public class SSHTest {
+	
+	@Resource
+	private Date date;
+	
+	@Test
+	public void date(){
+		System.out.println(date);
+	}
+}
